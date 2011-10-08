87363de73611bc00f750901906df676357c3d0eb
oliveira.ph17@gmail.com <oliveira.ph17@gmail.com@93325007-0316-a78f-de76-2317b6fd5304>
2011-10-08 00:46:16 +0000

    Apenas limpeza de código.

--- SysBus/src/java/beans/Trecho.java
+++ SysBus/src/java/beans/Trecho.java
@@ -1,13 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package beans;
 
-/**
- *
- * @author helioalb
- */
 public class Trecho {
     
 }
