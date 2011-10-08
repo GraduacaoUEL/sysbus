a4da80c804cf4659e4322c1901b96423d0de5403
oliveira.ph17@gmail.com <oliveira.ph17@gmail.com@93325007-0316-a78f-de76-2317b6fd5304>
2011-10-08 21:52:07 +0000

    Operações básicas em Trecho e TrechoDAO.

--- trunk/SysBus/src/java/DAO/TrechoDAO.java
+++ trunk/SysBus/src/java/DAO/TrechoDAO.java
@@ -1,5 +1,110 @@
 package DAO;
 
+import beans.Trecho;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class TrechoDAO {
-    
+
+    private Connection connection = null;
+    private PreparedStatement pstmt = null;
+
+    public TrechoDAO() {
+    }
+
+    private Connection getConnection() throws SQLException {
+        Connection conn;
+        conn = ConnectionFactory.getInstance().getConnection();
+        return conn;
+    }
+
+    public void add(Trecho trecho) {
+        try {
+            String queryString = "INSERT INTO trecho(origem_trecho,"
+                    + " destino_trecho, tempo_trecho, distancia_trecho"
+                    + " custo_trecho) VALUES(?, ?, ?, ?, ?)";
+            connection = getConnection();
+            pstmt = connection.prepareStatement(queryString);
+            pstmt.setString(1, trecho.getOrigemTrecho());
+            pstmt.setString(2, trecho.getDestinoTrecho());
+            pstmt.setTime(3, trecho.getTempoTrecho());
+            pstmt.setFloat(4, trecho.getDistanciaTrecho());
+            pstmt.setInt(5, trecho.getCustoTrecho());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void delete(int codigoTrecho) {
+        try {
+            String queryString = "DELETE FROM trecho WHERE codigo_trecho = ?";
+            connection = getConnection();
+            pstmt = connection.prepareStatement(queryString);
+            pstmt.setInt(1, codigoTrecho);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void update(Trecho trecho) {
+        try {
+            String queryString = "UPDATE trecho SET origem_trecho = ?,"
+                    + " destino_trecho = ?, tempo_trecho = ?,"
+                    + " distancia_trecho = ?, custo_trecho = ? WHERE"
+                    + " codigo_trecho = ?";
+            connection = getConnection();
+            pstmt = connection.prepareStatement(queryString);
+            pstmt.setString(1, trecho.getOrigemTrecho());
+            pstmt.setString(2, trecho.getDestinoTrecho());
+            pstmt.setTime(3, trecho.getTempoTrecho());
+            pstmt.setFloat(4, trecho.getDistanciaTrecho());
+            pstmt.setInt(5, trecho.getCustoTrecho());
+            pstmt.setInt(6, trecho.getCodigoTrecho());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- trunk/SysBus/src/java/beans/Trecho.java
+++ trunk/SysBus/src/java/beans/Trecho.java
@@ -1,5 +1,60 @@
 package beans;
 
+import java.sql.Time;
+
 public class Trecho {
-    
+
+    private int codigoTrecho;
+    private String origemTrecho;
+    private String destinoTrecho;
+    private Time tempoTrecho;
+    private float distanciaTrecho;
+    private int custoTrecho;
+
+    public Trecho() {
+    }
+
+    public int getCodigoTrecho() {
+        return codigoTrecho;
+    }
+
+    public String getOrigemTrecho() {
+        return origemTrecho;
+    }
+
+    public void setOrigemTrecho(String origemTrecho) {
+        this.origemTrecho = origemTrecho;
+    }
+
+    public String getDestinoTrecho() {
+        return destinoTrecho;
+    }
+
+    public void setDestinoTrecho(String destinoTrecho) {
+        this.destinoTrecho = destinoTrecho;
+    }
+
+    public Time getTempoTrecho() {
+        return tempoTrecho;
+    }
+
+    public void setTempoTrecho(Time tempoTrecho) {
+        this.tempoTrecho = tempoTrecho;
+    }
+
+    public float getDistanciaTrecho() {
+        return distanciaTrecho;
+    }
+
+    public void setDistanciaTrecho(float distanciaTrecho) {
+        this.distanciaTrecho = distanciaTrecho;
+    }
+
+    public int getCustoTrecho() {
+        return custoTrecho;
+    }
+
+    public void setCustoTrecho(int custoTrecho) {
+        this.custoTrecho = custoTrecho;
+    }
 }
