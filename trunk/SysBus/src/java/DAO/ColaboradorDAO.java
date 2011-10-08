49f7bf78c842f344522c2338ff54f8ebff7c9733
oliveira.ph17@gmail.com <oliveira.ph17@gmail.com@93325007-0316-a78f-de76-2317b6fd5304>
2011-10-08 20:47:07 +0000

    Operações básicas em Colaborador e ColaboradorDAO.

--- trunk/SysBus/src/java/DAO/ColaboradorDAO.java
+++ trunk/SysBus/src/java/DAO/ColaboradorDAO.java
@@ -1,5 +1,108 @@
 package DAO;
 
+import beans.Colaborador;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ColaboradorDAO {
-    
+
+    private Connection connection = null;
+    private PreparedStatement pstmt = null;
+
+    public ColaboradorDAO() {
+    }
+
+    private Connection getConnection() throws SQLException {
+        Connection conn;
+        conn = ConnectionFactory.getInstance().getConnection();
+        return conn;
+    }
+
+    public void add(Colaborador colaborador) {
+        try {
+            String queryString = "INSERT INTO colaborador(nome_colaborador,"
+                    + " login_colaborador, senha_colaborador, cargo_colaborador"
+                    + " VALUES(?, ?, ?, ?)";
+            connection = getConnection();
+            pstmt = connection.prepareStatement(queryString);
+            pstmt.setString(1, colaborador.getNomeColaborador());
+            pstmt.setString(2, colaborador.getLoginColaborador());
+            pstmt.setString(3, colaborador.getSenhaColaborador());
+            pstmt.setInt(4, colaborador.getCargoColaborador());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void delete(int codigoColaborador) {
+        try {
+            String queryString = "DELETE FROM colaborador WHERE"
+                    + " codigo_colaborador = ?";
+            connection = getConnection();
+            pstmt = connection.prepareStatement(queryString);
+            pstmt.setInt(1, codigoColaborador);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void update(Colaborador colaborador) {
+        try {
+            String queryString = "UPDATE colaborador SET nome_colaborador = ?,"
+                    + " login_colaborador = ?, senha_colaborador = ?,"
+                    + " cargo_colaborador = ? WHERE codigo_colaborador = ?";
+            connection = getConnection();
+            pstmt = connection.prepareStatement(queryString);
+            pstmt.setString(1, colaborador.getNomeColaborador());
+            pstmt.setString(2, colaborador.getLoginColaborador());
+            pstmt.setString(3, colaborador.getSenhaColaborador());
+            pstmt.setInt(4, colaborador.getCargoColaborador());
+            pstmt.setInt(5, colaborador.getCodigoColaborador());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- trunk/SysBus/src/java/beans/Colaborador.java
+++ trunk/SysBus/src/java/beans/Colaborador.java
@@ -1,5 +1,51 @@
 package beans;
 
-public class Colaborador {
-    
+import java.io.Serializable;
+
+public class Colaborador implements Serializable {
+
+    private int codigoColaborador;
+    private String nomeColaborador;
+    private String loginColaborador;
+    private String senhaColaborador;
+    private int cargoColaborador;
+
+    public Colaborador() {
+    }
+
+    public int getCodigoColaborador() {
+        return codigoColaborador;
+    }
+
+    public String getNomeColaborador() {
+        return nomeColaborador;
+    }
+
+    public void setNomeColaborador(String nomeColaborador) {
+        this.nomeColaborador = nomeColaborador;
+    }
+
+    public String getLoginColaborador() {
+        return loginColaborador;
+    }
+
+    public void setLoginColaborador(String loginColaborador) {
+        this.loginColaborador = loginColaborador;
+    }
+
+    public String getSenhaColaborador() {
+        return senhaColaborador;
+    }
+
+    public void setSenhaColaborador(String senhaColaborador) {
+        this.senhaColaborador = senhaColaborador;
+    }
+
+    public int getCargoColaborador() {
+        return cargoColaborador;
+    }
+
+    public void setCargoColaborador(int cargoColaborador) {
+        this.cargoColaborador = cargoColaborador;
+    }
 }
