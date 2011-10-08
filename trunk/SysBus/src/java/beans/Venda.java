671f17590051f53ac8933dc3defc81d31540531f
helioalb@gmail.com <helioalb@gmail.com@93325007-0316-a78f-de76-2317b6fd5304>
2011-10-08 20:53:36 +0000

    Implementado operações basicas de Venda.java e VendaDAO.java

--- trunk/SysBus/src/java/beans/Venda.java
+++ trunk/SysBus/src/java/beans/Venda.java
@@ -1,5 +1,79 @@
 package beans;
 
-public class Venda {
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Venda implements Serializable{
+    private int codigoVenda;
+    private Date dataVenda;
+    private String tipoPagamento;
+    private float valorVenda;
+    private boolean passagemEntregue;
+    private boolean viaWeb;
+    private int vendedor;
+    private int carroVenda;
     
+    public Venda() {
+        
+    }
+
+    public int getCodigoVenda() {
+        return codigoVenda;
+    }
+
+    public Date getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(Date dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+
+    public String getTipoPagamento() {
+        return tipoPagamento;
+    }
+
+    public void setTipoPagamento(String tipoPagamento) {
+        this.tipoPagamento = tipoPagamento;
+    }
+
+    public float getValorVenda() {
+        return valorVenda;
+    }
+
+    public void setValorVenda(float valorVenda) {
+        this.valorVenda = valorVenda;
+    }
+
+    public boolean isPassagemEntregue() {
+        return passagemEntregue;
+    }
+
+    public void setPassagemEntregue(boolean passagemEntregue) {
+        this.passagemEntregue = passagemEntregue;
+    }
+
+    public boolean isViaWeb() {
+        return viaWeb;
+    }
+
+    public void setViaWeb(boolean viaWeb) {
+        this.viaWeb = viaWeb;
+    }
+
+    public int getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(int vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public int getCarroVenda() {
+        return carroVenda;
+    }
+
+    public void setCarroVenda(int carroVenda) {
+        this.carroVenda = carroVenda;
+    }
 }
