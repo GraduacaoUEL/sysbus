4420a3f0c795687cd169ca506d3ff36311c3e2d0
oliveira.ph17@gmail.com <oliveira.ph17@gmail.com@93325007-0316-a78f-de76-2317b6fd5304>
2011-10-08 21:26:15 +0000

    Operações básicas em Custo e CustoDAO.

--- SysBus/src/java/beans/Custo.java
+++ SysBus/src/java/beans/Custo.java
@@ -1,5 +1,24 @@
 package beans;
 
-public class Custo {
-    
+import java.io.Serializable;
+
+public class Custo implements Serializable {
+
+    private int codigoCusto;
+    private float precoQuilometro;
+
+    public Custo() {
+    }
+
+    public int getCodigoCusto() {
+        return codigoCusto;
+    }
+
+    public float getPrecoQuilometro() {
+        return precoQuilometro;
+    }
+
+    public void setPrecoQuilometro(float precoQuilometro) {
+        this.precoQuilometro = precoQuilometro;
+    }
 }
