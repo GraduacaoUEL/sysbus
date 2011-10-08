2dd0fb23633e89abbec185461174a3dbb049b303
helioalb@gmail.com <helioalb@gmail.com@93325007-0316-a78f-de76-2317b6fd5304>
2011-10-08 18:19:30 +0000

    Adicionado ConnectionFactory com configuração do banco de dados, implementação do bean Carro e parte da implementação do CarroDAO.

---
+++ SysBus/src/java/DAO/ConnectionFactory.java
@@ -0,0 +1,35 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    String driverClassName = "org.postgresql.Driver";
+    String connectionUrl = "jdbc:postgresql://localhost:5432/sysbus";
+    String dbUser = "sysbus";
+    String dbPwd = "bd17";
+    
+    private static ConnectionFactory connectionFactory = null;
+    
+    private ConnectionFactory(){
+        try{
+            Class.forName(driverClassName);
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public Connection getConnection() throws SQLException{
+        Connection conn = null;
+        conn = DriverManager.getConnection(connectionUrl, dbUser, dbPwd);
+        return conn;
+    }
+    
+    public static ConnectionFactory getInstance(){
+        if(connectionFactory == null){
+            connectionFactory = new ConnectionFactory();
+        }
+        return connectionFactory;
+    }
+}
--- SysBus/src/java/beans/Carro.java
+++ SysBus/src/java/beans/Carro.java
@@ -1,5 +1,29 @@
 package beans;
 
-public class Carro {
+import java.io.Serializable;
+
+public class Carro implements Serializable{
+    private int id;
+    private int numeroDePassageiros;
+    
+    public Carro(){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumeroDePassageiros() {
+        return numeroDePassageiros;
+    }
+
+    public void setNumeroDePassageiros(int numeroDePassageiros) {
+        this.numeroDePassageiros = numeroDePassageiros;
+    }
     
 }
