9c5629510a800894139b70d1c0ec58eac7ec60d0
oliveira.ph17@gmail.com <oliveira.ph17@gmail.com@93325007-0316-a78f-de76-2317b6fd5304>
2011-10-08 20:22:48 +0000

    Operações básicas em Cargo e CargoDAO.

--- trunk/SysBus/src/java/beans/Cargo.java
+++ trunk/SysBus/src/java/beans/Cargo.java
@@ -1,5 +1,69 @@
 package beans;
 
-public class Cargo {
+import java.io.Serializable;
+
+public class Cargo implements Serializable {
+    private int codigoCargo;
+    private String nomeCargo;
+    private boolean permissaoCargos;
+    private boolean permissaoCarros;
+    private boolean permissaoItinerarios;
+    private boolean permissaoVendas;
+    private boolean permissaoCustos;
     
+    public Cargo() {
+        
+    }
+
+    public int getCodigoCargo() {
+        return codigoCargo;
+    }
+
+    public String getNomeCargo() {
+        return nomeCargo;
+    }
+
+    public void setNomeCargo(String nomeCargo) {
+        this.nomeCargo = nomeCargo;
+    }
+
+    public boolean isPermissaoCargos() {
+        return permissaoCargos;
+    }
+
+    public void setPermissaoCargos(boolean permissaoCargos) {
+        this.permissaoCargos = permissaoCargos;
+    }
+
+    public boolean isPermissaoCarros() {
+        return permissaoCarros;
+    }
+
+    public void setPermissaoCarros(boolean permissaoCarros) {
+        this.permissaoCarros = permissaoCarros;
+    }
+
+    public boolean isPermissaoItinerarios() {
+        return permissaoItinerarios;
+    }
+
+    public void setPermissaoItinerarios(boolean permissaoItinerarios) {
+        this.permissaoItinerarios = permissaoItinerarios;
+    }
+
+    public boolean isPermissaoVendas() {
+        return permissaoVendas;
+    }
+
+    public void setPermissaoVendas(boolean permissaoVendas) {
+        this.permissaoVendas = permissaoVendas;
+    }
+
+    public boolean isPermissaoCustos() {
+        return permissaoCustos;
+    }
+
+    public void setPermissaoCustos(boolean permissaoCustos) {
+        this.permissaoCustos = permissaoCustos;
+    }
 }
