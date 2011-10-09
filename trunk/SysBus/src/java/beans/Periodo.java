7f828c3d0a98f6c1a973958ec8ce7b1cd3a2364f
helioalb@gmail.com <helioalb@gmail.com@93325007-0316-a78f-de76-2317b6fd5304>
2011-10-09 11:09:39 +0000

    Implementado operações basicas de Periodo.java e PeriodoDAO.java

--- trunk/SysBus/src/java/beans/Periodo.java
+++ trunk/SysBus/src/java/beans/Periodo.java
@@ -1,5 +1,42 @@
 package beans;
 
+import java.sql.Time;
+
 public class Periodo {
     
+    private int codigoPeriodo;
+    private Time inicioPeriodo;
+    private Time fimPeriodo;
+    private float fatorMultiplicacao;
+    
+    public Periodo() {    
+    }
+
+    public int getCodigoPeriodo() {
+        return codigoPeriodo;
+    }
+
+    public Time getInicioPeriodo() {
+        return inicioPeriodo;
+    }
+
+    public void setInicioPeriodo(Time inicioPeriodo) {
+        this.inicioPeriodo = inicioPeriodo;
+    }
+
+    public Time getFimPeriodo() {
+        return fimPeriodo;
+    }
+
+    public void setFimPeriodo(Time fimPeriodo) {
+        this.fimPeriodo = fimPeriodo;
+    }
+
+    public float getFatorMultiplicacao() {
+        return fatorMultiplicacao;
+    }
+
+    public void setFatorMultiplicacao(float fatorMultiplicacao) {
+        this.fatorMultiplicacao = fatorMultiplicacao;
+    }
 }
