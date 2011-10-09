8f10ce9ae79fb4c9a7691d2b01b31268f4746000
helioalb@gmail.com <helioalb@gmail.com@93325007-0316-a78f-de76-2317b6fd5304>
2011-10-09 11:09:39 +0000

    Implementado operações basicas de Periodo.java e PeriodoDAO.java

--- SysBus/src/java/DAO/PeriodoDAO.java
+++ SysBus/src/java/DAO/PeriodoDAO.java
@@ -1,5 +1,110 @@
 package DAO;
 
+import beans.Periodo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class PeriodoDAO {
-    
+
+    private Connection connection = null;
+    private PreparedStatement pstmt = null;
+
+    public PeriodoDAO() {
+    }
+
+    private Connection getConnection() throws SQLException {
+        Connection conn;
+        conn = ConnectionFactory.getInstance().getConnection();
+        return conn;
+    }
+
+    public void add(Periodo periodo ) {
+        try {
+            String queryString = "INSERT INTO periodo(inicio_periodo,"
+                    + " fim_periodo, fator_multiplicacao) VALUES(?, ?, ?)";
+
+            connection = getConnection();
+
+            pstmt = connection.prepareStatement(queryString);
+            pstmt.setTime(1, periodo.getInicioPeriodo());
+            pstmt.setTime(2, periodo.getFimPeriodo());
+            pstmt.setFloat(3, periodo.getFatorMultiplicacao());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void delete(int codigoPeriodo) {
+        try {
+            String queryString = "DELETE FROM periodo WHERE codigo_periodo = ?";
+
+            connection = getConnection();
+
+            pstmt = connection.prepareStatement(queryString);
+            pstmt.setInt(1, codigoPeriodo);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void update(Periodo periodo) {
+        try {
+            String queryString = "UPDATE periodo SET inicio_periodo = ?,"
+                    + " fim_periodo = ?, fator_multiplicacao = ? WHERE"
+                    + " codigo_periodo = ?";
+
+            connection = getConnection();
+
+            pstmt = connection.prepareStatement(queryString);
+            pstmt.setTime(1, periodo.getInicioPeriodo());
+            pstmt.setTime(2, periodo.getFimPeriodo());
+            pstmt.setFloat(3, periodo.getFatorMultiplicacao());
+            pstmt.setInt(4, periodo.getCodigoPeriodo());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
