91bc9dd915a2202b4c25026dae34d96213f7f6fa
oliveira.ph17@gmail.com <oliveira.ph17@gmail.com@93325007-0316-a78f-de76-2317b6fd5304>
2011-10-09 15:04:48 +0000

    Alteração no fator de multiplicação do período.

--- SysBus/src/java/docs/SQL/Periodo.sql
+++ SysBus/src/java/docs/SQL/Periodo.sql
@@ -3,6 +3,6 @@ CREATE TABLE Periodo
     codigo_periodo INTEGER,
     inicio_periodo TIME NOT NULL,
     fim_periodo TIME NOT NULL,
-    fator_multiplicacao DECIMAL(2,2) NOT NULL,
+    fator_multiplicacao NUMERIC(3,2) NOT NULL,
 PRIMARY KEY(codigo_periodo)
 );
