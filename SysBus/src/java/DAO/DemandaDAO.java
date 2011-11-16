5c2866cca8dc62c85a3bba8898da25ef39df3991
oliveira.ph17@gmail.com <oliveira.ph17@gmail.com@93325007-0316-a78f-de76-2317b6fd5304>
2011-11-16 03:05:21 +0000

    Detalhes.

--- SysBus/src/java/DAO/DemandaDAO.java
+++ SysBus/src/java/DAO/DemandaDAO.java
@@ -12,7 +12,7 @@ public class DemandaDAO {
 
     private Connection connection = null;
     private PreparedStatement pstmt = null;
-    
+
     /**
      * Construtor vazio.
      */
@@ -24,14 +24,14 @@ public class DemandaDAO {
         conn = ConnectionFactory.getInstance().getConnection();
         return conn;
     }
-    
+
     /**
      * Insere uma demanda no banco de dados.
      * @param demanda Demanda a ser inserida.
      */
     public void insert(Demanda demanda) {
         try {
-            
+
             /*
              * Exemplo de expressão que será gerada
              * 
@@ -47,20 +47,21 @@ public class DemandaDAO {
              */
             String queryString = "BEGIN; ";
             queryString += "INSERT INTO demanda(nome_demanda) VALUES('" + demanda.getNomeDemanda() + "'); ";
-                        
-            for(int i = 0; i < demanda.getPeriodosDemanda().size(); i++)
+
+            for (int i = 0; i < demanda.getPeriodosDemanda().size(); i++) {
                 queryString += "INSERT INTO segue(numero_demanda, numero_periodo) "
                         + "VALUES((SELECT codigo_demanda FROM demanda WHERE "
                         + "nome_demanda = '" + demanda.getNomeDemanda() + "'),"
-                        + " "+ demanda.getPeriodosDemanda().get(i).getCodigoPeriodo() +"); ";
-            
+                        + " " + demanda.getPeriodosDemanda().get(i).getCodigoPeriodo() + "); ";
+            }
+
             queryString += "COMMIT;";
-            
+
             //Debug para ver como ficou a expressão gerada
             //System.out.println(queryString);
-                    
+
             connection = getConnection();
-            
+
             pstmt = connection.prepareStatement(queryString);
             pstmt.executeUpdate();
             System.out.println(queryString);
@@ -81,7 +82,7 @@ public class DemandaDAO {
             }
         }
     }
-    
+
     /**
      * Remove uma demanda do banco de dados.
      * @param codigoDemanda Código da demanda a ser removida.
@@ -89,9 +90,9 @@ public class DemandaDAO {
     public void delete(int codigoDemanda) {
         try {
             String queryString = "DELETE FROM demanda WHERE codigo_demanda = ?";
-            
+
             connection = getConnection();
-            
+
             pstmt = connection.prepareStatement(queryString);
             pstmt.setInt(1, codigoDemanda);
             pstmt.executeUpdate();
@@ -112,7 +113,7 @@ public class DemandaDAO {
             }
         }
     }
-    
+
     /**
      * Atualiza uma demanda no banco de dados.
      * @param demanda Demanda a ser atualizada.
@@ -121,9 +122,9 @@ public class DemandaDAO {
         try {
             String queryString = "UPDATE demanda SET nome_demanda = ? WHERE"
                     + " codigo_demanda = ?";
-            
+
             connection = getConnection();
-            
+
             pstmt = connection.prepareStatement(queryString);
             pstmt.setString(1, demanda.getNomeDemanda());
             pstmt.setInt(2, demanda.getCodigoDemanda());
@@ -145,49 +146,52 @@ public class DemandaDAO {
             }
         }
     }
-    
-     /**
+
+    /**
      * Retorna todos as tupas da entidade Cargo
      * @param cargo Cargo a ser atualizado.
      */
     public ArrayList<Demanda> selectAll() {
-    
-    ResultSet resultSet = null;
-    ArrayList<Demanda> demandas = new ArrayList<Demanda>();
-    
-    try {
-                        String queryString = "SELECT * FROM demanda";
-                        connection = getConnection();
-                        pstmt = connection.prepareStatement(queryString);
-                        resultSet = pstmt.executeQuery();
-                        while (resultSet.next()) {
-                            
-                            Demanda demanda = new Demanda();
-                            
-                            demanda.setCodigoDemanda(resultSet.getInt("codigo_demanda"));
-                            demanda.setNomeDemanda(resultSet.getString("nome_demanda"));
-                            
-                            demandas.add(demanda);
-                        }
-                        
-                        
-                } catch (SQLException e) {
-                        e.printStackTrace();
-                } finally {
-                        try {
-                                if (resultSet != null)
-                                        resultSet.close();
-                                if (pstmt != null)
-                                        pstmt.close();
-                                if (connection != null)
-                                        connection.close();
-                        } catch (SQLException e) {
-                                e.printStackTrace();
-                        } catch (Exception e) {
-                                e.printStackTrace();
-                        }
 
+        ResultSet resultSet = null;
+        ArrayList<Demanda> demandas = new ArrayList<Demanda>();
+
+        try {
+            String queryString = "SELECT * FROM demanda";
+            connection = getConnection();
+            pstmt = connection.prepareStatement(queryString);
+            resultSet = pstmt.executeQuery();
+            while (resultSet.next()) {
+
+                Demanda demanda = new Demanda();
+
+                demanda.setCodigoDemanda(resultSet.getInt("codigo_demanda"));
+                demanda.setNomeDemanda(resultSet.getString("nome_demanda"));
+
+                demandas.add(demanda);
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
                 }
-                return demandas;
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
+        return demandas;
+    }
 }
